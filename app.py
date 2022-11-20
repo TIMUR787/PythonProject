8d0a61986bab190536cf1f1bb41624ac71ceaf1c
Портнов Тимур Алексеевич <113140011+TIMUR787@users.noreply.github.com>
2022-11-20 18:12:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from Windows import *
+
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE, pygame.FULLSCREEN)
+
+window_menu = WindowMenu(screen)
+while True:
+    window_menu.run()
+
+pygame.quit()
+exit()
